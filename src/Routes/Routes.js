10d101fed86efe1d06de39f380688db532ef7587
Istiakahmed180 <Istiakahmed180@gmail.com>
2022-10-25 01:12:58 +0600

    added hero component for home route

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainRoute from "../MainRoute/MainRoute";
+import HeroSection from "../Pages/HeroSection/HeroSection";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainRoute></MainRoute>,
+    children: [
+      {
+        path: "/",
+        element: <HeroSection></HeroSection>,
+      },
+    ],
   },
 ]);
 
